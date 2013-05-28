498f2066f035ea1cccdb7b7bb3586c4c523791e2
Max Shytikov <mshytikov@gmail.com>
2013-05-28 19:20:04 +0300

    add population count init impl;

--- lib/bits_count.rb
+++ lib/bits_count.rb
@@ -5,8 +5,13 @@ module BitsCount
   #
   
   class << self
-    def population_count(i)
-      0
+    def population_count(x)
+      x = x - ((x >> 1) & 0x55555555)
+      x = (x & 0x33333333) + ((x >> 2) & 0x33333333)
+      x = (x + (x >> 4)) & 0x0F0F0F0F
+      x = x + (x >> 8)
+      x = x + (x >> 16)
+      x & 0x0000003F
     end
   end
 end
--- test/test_bits_count.rb
+++ test/test_bits_count.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class TestBitsCount < Test::Unit::TestCase
 
-  def test_hamming_weight
+  def test_population_count
+    assert_equal(0, BitsCount.population_count(0))
     assert_equal(1, BitsCount.population_count(1))
+    assert_equal(2, BitsCount.population_count(3))
   end
+
 end
